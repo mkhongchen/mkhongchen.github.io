f017fa29de5276a09fa8a6ab13d661b24b27b5e6
mkhongchen <mkhongchen@gmail.com>
2025-08-13 16:43:00 -0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  let name = "Ming Kang Chen";
+  let tagline = "Aspiring architect and designer. Website under development";
+</script>
+
+<main>
+  <h1>{name}</h1>
+  <p>{tagline}</p>
+  <!-- svelte-ignore a11y_img_redundant_alt -->
+  <img src="/profile.jpg" alt="Profile photo" />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    margin-top: 4rem;
+  }
+  img {
+    width: 150px;
+    border-radius: 50%;
+  }
+</style>
